e9716ce4f05bde166f37dc3e138f088791de98fa
AmolSKhare <amolsanjaykhare@gmail.com>
2024-09-03 07:35:45 +0530

    updated the gitbash commands and added 2 simple examples

---
+++ javascript/example1.js
@@ -0,0 +1,8 @@
+//what will be the output 
+
+let a =20 ;
+function log(){
+    console.log("number is = ",a);
+}
+a =10;
+log();
---
+++ javascript/example2.js
@@ -0,0 +1,5 @@
+const func =()=>{
+    let a =5>3>2
+    return !a;
+}
+console.log(func());
